1006a10cabbe992589efa2ddb383330c59519adc
Jadon <1520173329@mail.jxust.edu.cn>
2021-03-18 21:20:07 +0800

    初步完成

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
 import './axios.js'
+import './premission.js'
 // import './autoload.js'
 // import './jquery.min.js'
 // import './semantic.min.js'
--- src/router/index.js
+++ src/router/index.js
@@ -8,50 +8,56 @@ import BlogDetail from '../views/BlogDetail.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'index',
-    redirect: {name:"Blogs"}
-  },
-  {
-    path: '/blogs',
-    name: 'Blogs',
-    component: Blogs
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/blog/add',
-    name: 'BlogAdd',
-    component: BlogEdit
-  },
-  {
-    path: '/blog/:blogId',
-    name: 'BlogDetail',
-    component: BlogDetail
-  },
-  {
-    path: '/blog/:blogId/edit',
-    name: 'BlogEdit',
-    component: BlogEdit
-  }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+    {
+        path: '/',
+        name: 'index',
+        redirect: {name: "Blogs"}
+    },
+    {
+        path: '/blogs',
+        name: 'Blogs',
+        component: Blogs
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/blog/add',
+        name: 'BlogAdd',
+        component: BlogEdit,
+        meta: {
+            requireAuth: true
+        }
+    },
+    {
+        path: '/blog/:blogId',
+        name: 'BlogDetail',
+        component: BlogDetail
+    },
+    {
+        path: '/blog/:blogId/edit',
+        name: 'BlogEdit',
+        component: BlogEdit,
+        meta: {
+            requireAuth: true
+        }
+    }
+    // {
+    //   path: '/about',
+    //   name: 'About',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
